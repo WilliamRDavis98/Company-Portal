5b3505213adb3ad7a483974ce0c94c4770e5cb47
Bobby Hodge <bobbyhodge2@gmail.com>
2023-11-20 12:38:17 -0500

    Assessment 3: UI Team - Basic Page setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SelectCompanyComponent } from './select-company/select-company.component';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UserRegistryComponent } from './user-registry/user-registry.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CreateProjectComponent } from './components/modals/create-project/create-project.component';
+import { CreateTeamComponent } from './components/modals/create-team/create-team.component';
+import { CreateEditProjectComponent } from './components/modals/create-edit-project/create-edit-project.component';
+import { CreateUserComponent } from './components/modals/create-user/create-user.component';
+import { AnnouncementComponent } from './home/announcement/announcement.component';
+import { TeamComponent } from './teams/team/team.component';
+import { ProjectComponent } from './projects/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SelectCompanyComponent,
+    HomeComponent,
+    TeamsComponent,
+    ProjectsComponent,
+    UserRegistryComponent,
+    NavBarComponent,
+    CreateProjectComponent,
+    CreateTeamComponent,
+    CreateEditProjectComponent,
+    CreateUserComponent,
+    AnnouncementComponent,
+    TeamComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/modals/create-edit-project/create-edit-project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-edit-project',
+  templateUrl: './create-edit-project.component.html',
+  styleUrls: ['./create-edit-project.component.css']
+})
+export class CreateEditProjectComponent {
+
+}
