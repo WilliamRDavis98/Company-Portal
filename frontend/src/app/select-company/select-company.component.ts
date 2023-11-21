fcd46909c4236179a6cb8072023c3caca87c15fd
Mika Nelson <mika.nelson@protonmail.com>
2023-11-21 13:06:41 -0500

    rebase dev onto mika-feature

--- frontend/src/app/select-company/select-company.component.ts
+++ frontend/src/app/select-company/select-company.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-select-company',
   templateUrl: './select-company.component.html',
-  styleUrls: ['./select-company.component.css']
+  styleUrls: ['./select-company.component.css'],
 })
 export class SelectCompanyComponent {
-
+  companies = ['chik-fil-a', 'wal-mart', 'fedex'];
+  handleSelectCompany = (company: string) => {
+    console.log(company);
+  };
+  rotateIcon = () => {
+    let icon = document.getElementById('icon');
+    if (icon && icon.style.transform === '') {
+      icon.style.transform = 'rotate(180deg)';
+    } else if (icon) {
+      icon.style.transform = '';
+    }
+  };
 }
