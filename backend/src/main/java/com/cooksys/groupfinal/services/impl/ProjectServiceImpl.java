c1f8d2dc66db9756a7669adbf40e69b08a2ebf7c
John McConnell <john.mcconnell207@gmail.com>
2023-11-22 13:38:56 -0500

    patch project endpoint created

--- backend/src/main/java/com/cooksys/groupfinal/services/impl/ProjectServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/ProjectServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cooksys.groupfinal.services.impl;
 import com.cooksys.groupfinal.exceptions.NotFoundException;
 import java.util.Optional;
+
+import com.cooksys.groupfinal.mappers.TeamMapper;
 import org.springframework.stereotype.Service;
 import com.cooksys.groupfinal.dtos.ProjectDto;
 import com.cooksys.groupfinal.dtos.ProjectRequestDto;
@@ -23,6 +25,8 @@ public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
 
+    private final TeamMapper teamMapper;
+
     @Override
     public ProjectDto findProjectById(Long id) {
         Optional<Project> projectToFind = projectRepository.findById(id);
@@ -34,5 +38,28 @@ public class ProjectServiceImpl implements ProjectService {
         // might add an isActive error handling as well
 
         return projectMapper.entityToDto(projectToFind.get());
+    }
+
+    @Override
+    public ProjectDto updateProject(Long id, ProjectDto projectDto) {
+        // I could also get the id from the project Dto if we want to limit the variables passed in
+        Optional<Project> projectToFind = projectRepository.findById(id);
+        if (projectToFind.isEmpty()) {
+            throw new NotFoundException("This project doesn't exist");
+        }
+        Project projectToEdit = projectToFind.get();
+        projectToEdit.setName(projectDto.getName());
+        // description, active, lastUpdated, team
+        projectToEdit.setDescription(projectDto.getDescription());
+        projectToEdit.setLastUpdated(projectDto.getLastUpdated());
+        // was thinking of getting lastUpdated from the frontend, but I could also probably set a timestamp with the current time
+        // would look like this
+        // Date currentDate = new Date();
+        // projectToEdit.setLastUpdated(currentDate.getTime());
+        projectToEdit.setTeam(teamMapper.dtoToEntity(projectDto.getTeam()));
+        // maybe I add a save team functionality as well, or do an edit team, don't think its neccessary tho
+        projectRepository.saveAndFlush(projectToEdit);
+
+        return projectDto;
     }
 }
