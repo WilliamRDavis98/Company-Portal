e0c5bd90a4138440c78551571221f330f2388320
John McConnell <john.mcconnell207@gmail.com>
2023-11-22 16:31:59 -0500

    enable all origins for api calls

--- backend/src/main/java/com/cooksys/groupfinal/controllers/advice/ValidateController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/advice/ValidateController.java
@@ -2,14 +2,12 @@ package com.cooksys.groupfinal.controllers.advice;
 
 import com.cooksys.groupfinal.services.ValidateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/validate")
+@CrossOrigin(origins="*")
 public class ValidateController {
 
     private final ValidateService validateService;
