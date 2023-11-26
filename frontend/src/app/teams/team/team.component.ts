52e5a276d3cb8d26526b32107fe1784a419e228d
Visrael24 <valorisrael@gmail.com>
2023-11-26 12:18:12 -0700

    teams page and team component

--- frontend/src/app/teams/team/team.component.ts
+++ frontend/src/app/teams/team/team.component.ts
@@ -1,10 +1,22 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit {
   @Input() teamData: any;
+
+  members: string[] = [];
+
+  ngOnInit(): void {
+    if (this.teamData && this.teamData.description) { // This is ensuring whitespace and empty names are omitted.
+      const separators = [',', '&'];
+      const regex = new RegExp(separators.join('|'), 'g');
+      this.members = this.teamData.description.split(regex)
+        .map((s: string) => s.trim())
+        .filter((name: string) => name !== "");
+    }
+  }
 }
--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
-import {TeamComponent} from "./team/team.component";
+import { Component, OnInit } from '@angular/core';
+import { ApiCallsService } from '../services/api-calls.service';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.css']
 })
-export class TeamsComponent {
+export class TeamsComponent implements OnInit {
+  teams: any[] = [];
+  CompanyID: any | null;
+  openNewTeamModal(): void {}
 
-  protected readonly TeamComponent = TeamComponent;
+  constructor(private apiCallsService: ApiCallsService) {}
 
-  //   GET companies/{id}/teams
-  //   Get Company ID from Session Storage
-
-  teams = [
-    {a: 1},
-    {a: 1},
-    {a: 1},
-  ];
-
-
-
+  ngOnInit() {
+    // this.CompanyID = sessionStorage.getItem('userCompany') || null;
+    // temp for testing
+    this.CompanyID = 6
+    if (this.CompanyID) {
+      this.apiCallsService.getTeamsByCompanyId(this.CompanyID).subscribe(
+        (teamsData) => {
+          this.teams = teamsData;
+          this.teams.forEach((team) => {
+            this.apiCallsService.getProjectsByTeamId(this.CompanyID, team.id).subscribe(
+              (projects) => {
+                team.projectsCount = projects.length;
+              },
+              (error) => {
+                console.error('Error fetching projects for team', team.id, error);
+              }
+            );
+          });
+        },
+      );
+    }
+  }
 }
